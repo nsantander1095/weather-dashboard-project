29942086df4b6ab6eb668f2432288a066c2554a3
Nick <nsantander1095@gmail.com>
2022-08-29 21:18:31 -0400

    got everything working. going to deploy and turn in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var appid = 'aa9a3cc1acf925497a48dd50cbf62287'; // API KEY 1: 485bbc753e29e9770f09ca55c32c6d79 API KEY 2: a83cd16f45a517250a52f83592bb80be
-var q = 'Charlotte';
+var q = '';
 var searchBtnEl = document.querySelector('#search');
+var previousCitiesEl = document.querySelector('#previousCities');
 
-var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
 var toJSON = function(response) {
     return response.json();
@@ -11,13 +11,14 @@ var toJSON = function(response) {
 var displayWeather = function (data, city) {
     console.log(data);
     var currentWeatherLocale = document.getElementById('currentWeather');
+    currentWeatherLocale.innerHTML = null;
     var h2El = document.createElement('h2');
     var tempEl = document.createElement('p');
     var windEl = document.createElement('p');
     var humidityEl = document.createElement('p');
     var uvIndexEl = document.createElement('p');
     h2El.textContent = city.name + " (" + moment(data.dt).format('M/D/YYYY') + ")";
-    tempEl.textContent = "Temp: " + data.current.temp + "\xB0 F";
+    tempEl.textContent = "Temp: " + data.current.temp + "\xB0F";
     windEl.textContent = "Wind: " + data.current.wind_speed + "MPH";
     humidityEl.textContent = "Humidity: " + data.current.humidity + "%";
     uvIndexEl.textContent = "UV Index: " + data.current.uvi;
@@ -26,6 +27,17 @@ var displayWeather = function (data, city) {
     currentWeatherLocale.appendChild(windEl);
     currentWeatherLocale.appendChild(humidityEl);
     currentWeatherLocale.appendChild(uvIndexEl);
+}
+
+var displayPrevButtons = function () {
+    var cities = JSON.parse(localStorage.getItem('cities')) || [];
+    previousCitiesEl.innerHTML = null;
+    for (var city of cities) {
+        var cityButtonEl = document.createElement('button');
+        cityButtonEl.textContent = city;
+        cityButtonEl.className = 'btn btn-secondary mb-3 btn-block';
+        previousCitiesEl.appendChild(cityButtonEl);
+    }
 }
 
 var getOneCall = function(city) {
@@ -39,11 +51,20 @@ var getOneCall = function(city) {
         })
 }
 
+var saveToLocalStorage = function (city) {
+    var cities = JSON.parse(localStorage.getItem('cities')) || [];
+    cities.push(city);
+    var citySet = Array.from(new Set(cities));
+    var data = JSON.stringify(citySet);
+    localStorage.setItem('cities', data);
+    displayPrevButtons(); 
+}
+
 var getGeo = function (locations) {
     var city = locations[0]
     console.log('Lat', city.lat);
     console.log('Lon', city.lon);
-
+    saveToLocalStorage(city.name);
     getOneCall(city);
 }
 
@@ -54,20 +75,25 @@ var displayForecast = function (data) {
     h3El.className = 'col-lg-12';
     h3El.textContent = "5-Day Forecast:";
     forecastEl.appendChild(h3El);
-
+    forecastEl.innerHTML = null;
     for (var i = 1; i < 6; i++) {
         var cardEl = document.createElement('div');
-        cardEl.className = 'card';
-        cardEl.style.width = '10rem';
         var cardBodyEl = document.createElement('div');
-        cardBodyEl.className = 'card-body';
-        var h4El = document.createElement('h4')
-        h4El.className = 'card-title';
+        cardBodyEl.innerHTML = null;
+        var h4El = document.createElement('h4');
         var cardTextEl = document.createElement('div');
-        cardTextEl.className = 'card-text';
         var tempEl = document.createElement('p');
         var windEl = document.createElement('p');
         var humidityEl = document.createElement('p');
+        var iconEl = document.createElement('img');
+        var icon = data.daily[i].weather[0].icon;
+        iconEl.alt = icon;
+        iconEl.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        cardEl.className = 'card m-3';
+        cardEl.style.width = '10rem';
+        cardBodyEl.className = 'card-body';
+        h4El.className = 'card-title';
+        cardTextEl.className = 'card-text';
         h4El.textContent = moment(data.daily[i].dt, 'X').format('M/D/YYYY');
         tempEl.textContent = "Temp: " + data.daily[i].temp.day + "\xB0 F";
         windEl.textContent = "Wind: " + data.daily[i].wind_speed + "MPH";
@@ -75,6 +101,7 @@ var displayForecast = function (data) {
         forecastEl.appendChild(cardEl);
         cardEl.appendChild(cardBodyEl);
         cardBodyEl.appendChild(h4El);
+        cardBodyEl.appendChild(iconEl);
         cardBodyEl.appendChild(cardTextEl);
         cardTextEl.appendChild(tempEl);
         cardTextEl.appendChild(windEl);
@@ -82,19 +109,30 @@ var displayForecast = function (data) {
     }
 }
 
-// var renderLastCities = function () {
+var searchHandler = function (event) {
+    event.preventDefault();
 
-// }
+    if (event.target.matches('button')) {
+        q = document.querySelector('#cityInput');
+        var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q.value}&appid=${appid}`;
+    
+        fetch(geoURL)
+            .then(toJSON)
+            .then(getGeo);
+    }
+}
 
-fetch(geoURL)
-    .then(toJSON)
-    .then(getGeo);
+var prevCityHandler = function (event) {
+    event.preventDefault();
+    q = event.target.textContent;
+    var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${appid}`;
 
+    fetch(geoURL)
+        .then(toJSON)
+        .then(getGeo);
+}
 
-    searchBtnEl.addEventListener('click', function() {
-        q = document.querySelector('#cityInput').value;
-        console.log(q);
-        localStorage.setItem('q', q);
-        
-    })
-   
+searchBtnEl.addEventListener('click', searchHandler);
+previousCitiesEl.addEventListener('click', prevCityHandler);
+
+displayPrevButtons();
